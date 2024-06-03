ae795e4d30adb5bb6029b0162cafe8719a433c69
Christopher Andrew <christopherandrew@ChristophersMBP.lan>
2024-06-03 19:35:01 +0800

    base code

--- filtr/filtr.py
+++ filtr/filtr.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import pandas as pd
+import sys
+
+#path = "/Users/christopherandrew/Documents/python-bash-scripting/filtr/file_to_filter/c-level_contacts.csv"
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+
+def main():
+  if len(sys.argv) != 3:
+    print("Usage: python filtr.py /path/to/file <'filters in single quotes'>")
+    return
+
+if __name__ == "__main__":
+  main()
