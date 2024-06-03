a0ce7ce5a3865030e2cd5d1948f542f10bc53f1b
Christopher Andrew <christopherandrew@ChristophersMBP.lan>
2024-06-03 20:00:41 +0800

    pd file read

--- filtr/filtr.py
+++ filtr/filtr.py
@@ -2,13 +2,20 @@ import pandas as pd
 import sys
 
 #path = "/Users/christopherandrew/Documents/python-bash-scripting/filtr/file_to_filter/c-level_contacts.csv"
-arg1 = sys.argv[1]
-arg2 = sys.argv[2]
 
 def main():
-  if len(sys.argv) != 3:
-    print("Usage: python filtr.py /path/to/file <'filters in single quotes'>")
+
+  if len(sys.argv) != 4:
+    print("Usage: python filtr.py '/path/to/file' 'col_to_filter' 'filters'")
     return
+  
+  arg1 = sys.argv[1]
+  arg2 = sys.argv[2]
+  arg3 = sys.argv[3]
+
+  data = pd.read_csv(arg1)
+  df = pd.DataFrame(data)
+
 
 if __name__ == "__main__":
   main()
